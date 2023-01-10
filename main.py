c998d1b5a49262778d791eb77b27ce7bd6383d12
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-10 19:10:17 +0000

    add logic that you have 5 attempts"

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import random
 
 
 chosen_number = random.randint(0,100)
+
+live = 5
+is_game_over = False
+while not is_game_over:
+    if live < 1:
+        print('-----------')
+        print('you lose.')
+        print('-----------')
+        is_game_over = True
+        
