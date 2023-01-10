c9d676088c49a0a5661e69627b8b85d23de78d1c
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-10 19:12:54 +0000

    add if statement that say theguess nnumber is larger or smaller than the chosen number

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ while not is_game_over:
         print('you lose.')
         print('-----------')
         is_game_over = True
+    else:
+        user_guess = int(input(f'enter number(you have {live}): '))
+        if user_guess == chosen_number:
+            print('well done your right!')
+            is_game_over = True
         
+        if user_guess < chosen_number:
+            print("it's Larger")
+            live -= 1
+        elif user_guess > chosen_number:
+            live -= 1
+            print("it's smaller")
